1f46d103bcebbdd824ad58c6928173c2ec7c06a3
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-22 07:54:12 -0300

    Retomar to topico sobre coercao explicita da aula de tipos

---
+++ aula_tipos.R
@@ -0,0 +1,125 @@
+x <- 5;
+x
+
+y <- #
+  
+  +
+  
+  + 2
+
+print(y)
+
+ls()
+
+rm(x)
+
+
+ls()
+
+rm(list = ls())
+
+ls()
+
+class ("Olá")
+
+class ('a')
+
+class (1)
+
+
+class (1L)
+
+class (TRUE)
+
+class (FALSE)
+
+class (1 + 4i)
+
+
+class(2i)
+
+1 / 0
+
+
+1 / Inf
+
+
+0 / 0
+
+class(1)
+
+class(1L)
+
+is.numeric(12)
+
+is.integer(15.4)
+
+is.integer("12")
+
+class (1L)
+x <- c (0.5, 1)
+x
+class(x[1])
+
+
+x <- c (TRUE, FALSE, FALSE)
+x
+
+y <- c (T, F)
+y
+class(T)
+
+c ("a" ,'b', 'c')
+
+
+x <- c (12L, -3L, 7)
+class(x[1])
+x <- c(-2L)
+class(x[1])
+class(x[0])
+x[0]
+
+1:10
+
+10:20
+
+-3:3
+
+3:-3
+
+3.2:10
+
+4.5:6
+
+
+seq(from = 1, to = 10, by = 2)
+
+
+seq (from = 3, to = -3, by = 1)
+
+rep (x = 1, times = 5)
+
+rep (x = c(1, 2, 3), times = 2)
+
+
+rep (x = 5, times = 3, each = 3)
+
+c (1.3, 2.1, 1.7, T, 1.2)
+
+
+c (1.3, 2.1, 1.7, "a", 1.2)
+
+c(TRUE, 2, FALSE)
+
+
+
+x <- -1.3:3;x
+as.integer(x)
+as.logical(x)
+as.logical(c(0, 1))
+as.logical(c(0, 1, 2, 3, 0))
+as.logical(c(-1, 0))
+
+x <- -1.3:3;x
+
+y <- as.character(x);y
